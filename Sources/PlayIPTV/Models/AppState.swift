7217cc786ffbd3de521f1c2174ff3a6bfe8bbf5c
inkee <baetony@gmail.com>
2025-12-25 00:52:14 -0500

    fix: clear search on category change

--- Sources/PlayIPTV/Models/AppState.swift
+++ Sources/PlayIPTV/Models/AppState.swift
@@ -61,6 +61,7 @@ class AppState {
         didSet {
             if oldValue?.id != selectedCategory?.id {
                 searchText = ""
+                channelSearchText = ""  // Also clear channel search when category changes
             }
         }
     }
